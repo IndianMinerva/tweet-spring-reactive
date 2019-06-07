52154c03361b5d3f81acab0864b73fc882443de1
Arun Merum <Arun.Merum@windeln.de>
2019-06-07 12:22:43 +0200

    Enabled Swagger documentation.

--- src/main/java/com/im/penguin/PenguinApplication.java
+++ src/main/java/com/im/penguin/PenguinApplication.java
@@ -2,8 +2,11 @@ package com.im.penguin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class PenguinApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/im/penguin/controller/TweetController.java
+++ src/main/java/com/im/penguin/controller/TweetController.java
@@ -2,14 +2,23 @@ package com.im.penguin.controller;
 
 import com.im.penguin.mode.Tweet;
 import com.im.penguin.repository.TweetRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@Api("Tweets Controller")
 public class TweetController {
 
     private final TweetRepository tweetRepository;
@@ -20,23 +29,28 @@ public class TweetController {
     }
 
     @GetMapping("/")
+    @ApiOperation("Get All Tweets")
     public Flux<Tweet> getAllTweets() {
         return tweetRepository.findAll();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Get a give Tweet")
     public Mono<ResponseEntity<Tweet>> getTweetById(@PathVariable(value = "id") String id) {
         return tweetRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
+
     @PostMapping("/")
+    @ApiOperation("Create a tweet")
     public Mono<Tweet> createTweet(@RequestBody Tweet tweet) {
         return tweetRepository.save(tweet);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation("Update a tweet")
     public Mono<ResponseEntity<Tweet>> updateTweet(@PathVariable(value = "id") String id, @RequestBody Tweet tweet) {
         return tweetRepository.findById(id)
                 .flatMap(existingTweet -> {
@@ -47,11 +61,12 @@ public class TweetController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation("Delete a tweet")
     public Mono<ResponseEntity<Void>> deleteTweet(@PathVariable(value = "id") String id) {
         return tweetRepository.findById(id)
                 .flatMap(existingTweet -> tweetRepository.delete(existingTweet)
                         .then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK)))
                 )
-                        .defaultIfEmpty(ResponseEntity.notFound().build());
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
